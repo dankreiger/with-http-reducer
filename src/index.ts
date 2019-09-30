ea8bdb95050f3045ebeb54a5b8c91ee7695c6719
Dan Kreiger <dan@dankreiger.com>
2019-09-30 20:21:46 +0200

    chore(*): syntax

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { HTTP_BEGIN, HTTP_FAILURE, HTTP_SUCCESS } from './actionTypeFormatters';
 import { withHttpReducer } from './withHttpReducer';
 
-export const httpAction = (reducerName?: string) => ({
+const httpAction = (reducerName?: string) => ({
   BEGIN: HTTP_BEGIN(reducerName),
   FAILURE: HTTP_FAILURE(reducerName),
   SUCCESS: HTTP_SUCCESS(reducerName)
 });
-
-export const withHttpReducer = htr;
 
 const reduxAsync = { httpAction, withHttpReducer };
 
