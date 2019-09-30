f5fca42006a9bfcd4a9592a4046aeb54678745a0
Dan Kreiger <dan@dankreiger.com>
2019-09-30 19:46:01 +0200

    fix(exports): update exported functions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { HTTP_BEGIN, HTTP_FAILURE, HTTP_SUCCESS } from "./actionTypeFormatters";
-import withHttpReducer from "./withHttpReducer";
+import { HTTP_BEGIN, HTTP_FAILURE, HTTP_SUCCESS } from './actionTypeFormatters';
+import withHttpReducer from './withHttpReducer';
 
-export * from "./withHttpReducer";
-export * from "./actionTypeFormatters";
+export * from './withHttpReducer';
+export * from './actionTypeFormatters';
 
 export const httpAction = (reducerName?: string) => ({
   BEGIN: HTTP_BEGIN(reducerName),
@@ -10,4 +10,4 @@ export const httpAction = (reducerName?: string) => ({
   SUCCESS: HTTP_SUCCESS(reducerName)
 });
 
-export default withHttpReducer;
+export default { httpAction, withHttpReducer };
